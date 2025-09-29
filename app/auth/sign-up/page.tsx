a00c485f7187f4debde4c5fb579c8e8f4fb01330
code.tejas <160333816+codetejass@users.noreply.github.com>
2025-09-30 00:18:21 +0530

    Update page.tsx

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -39,7 +39,7 @@ export default function SignUpPage() {
         password,
         options: {
           emailRedirectTo:
-            process.env.NEXT_PUBLIC_SUPABASE_SIGNUP_REDIRECT || `${window.location.origin}/auth/sign-up-success`,
+            process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || `${window.location.origin}/auth/sign-up-success`,
         },
       })
       if (signUpError) throw signUpError
